cc3e90763bf2563eb8d861fccabbd1dcd11418a5
Glauco Godoi <Glauco.Godoi@seila.com>
2021-10-18 21:09:19 +0100

    Cleanning

--- projects/client/src/environments/environment.prod.ts
+++ projects/client/src/environments/environment.prod.ts
@@ -2,12 +2,12 @@ export const environment = {
   production: true,
   useEmulators: false,
   firebaseConfig: {
-    apiKey: "AIzaSyBG-_zw6_S9p1pirTgBWSpzaLmRzLiBh2Q",
-    authDomain: "utilities-counter.firebaseapp.com",
-    projectId: "utilities-counter",
-    storageBucket: "utilities-counter.appspot.com",
-    messagingSenderId: "268930229420",
-    appId: "1:268930229420:web:f18eac3a3da51d441feb09",
-    measurementId: "G-WGF4MBBCQX"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
   }
 };
